f841a67975bec83502ae495b4c4f13545b00aa29
Bakhtiyor Abdullaev <bakhtiyor.abdullaev.frontend@gmail.com>
2023-02-13 03:41:08 +0500

    styling

--- main.js
+++ main.js
@@ -10,37 +10,50 @@ let info = fetch("https://fakestoreapi.com/products")
     console.log(allInformations);
     allInformations.map((item) => {
       let box = document.createElement("div");
-      box.classList.add("box");
+      box.classList.add("product-card");
 
-      let title = document.createElement("h1");
-      title.classList.add("title");
-      title.textContent = item.title;
-      let imageBox = document.createElement("div");
-      imageBox.classList.add("image");
+      let category = document.createElement("p");
+      category.classList.add("category");
+      category.textContent = item.category;
+
+      let miniBox = document.createElement("div");
+      miniBox.classList.add("mini-box");
 
       let image = document.createElement("img");
       image.src = item.image;
 
-      let ratingBox = document.createElement("div");
-      ratingBox.classList.add("rating");
+      let cardInfo = document.createElement("div");
+      cardInfo.classList.add("card-info");
 
-      let ratingStar = document.createElement("i");
-      ratingStar.classList.add("fa", "fa-star");
+      let title = document.createElement("p");
+      title.classList.add("title");
+      title.textContent = `Product: ${item.title}`;
 
-      let ratingNumber = document.createElement("span");
-      ratingNumber.item?.rating.rate;
-
-      let name = document.createElement("p");
-      name.classList.add("name");
-      name.textContent = item.title;
+      let rating = document.createElement("p");
+      rating.classList.add("rating");
+      rating.textContent = `Rating: ${item.rating.rate} (${item.rating.count})`;
 
       let price = document.createElement("p");
       price.classList.add("price");
-      price.textContent = `${item.price}$`;
+      price.textContent = `Price: ${item.price}$`;
 
-      box.appendChild(image);
-      box.appendChild(name);
-      box.appendChild(price);
+      let description = document.createElement("p");
+      description.classList.add("description");
+      description.textContent = `Description: ${item.description}`;
+
+      let button = document.createElement("button");
+      button.classList.add("button");
+      button.textContent = "Buy";
+
+      box.appendChild(category);
+      box.appendChild(miniBox);
+      miniBox.appendChild(image);
+      miniBox.appendChild(cardInfo);
+      cardInfo.appendChild(title);
+      cardInfo.appendChild(rating);
+      cardInfo.appendChild(price);
+      box.appendChild(description);
+      box.appendChild(button);
       container.appendChild(box);
     });
   });
